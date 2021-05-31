9a03a7611336fe60d467a24c9075ec94d19830f9
Ludovic_Casse <77667511+KsLudovic@users.noreply.github.com>
2021-05-31 15:29:28 +0200

    Initial commit

---
+++ src/main/java/fr/aston/snipcave/snipcave/SnipcaveApplication.java
@@ -0,0 +1,13 @@
+package fr.aston.snipcave.snipcave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnipcaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnipcaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/aston/snipcave/snipcave/SnipcaveApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.aston.snipcave.snipcave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnipcaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
