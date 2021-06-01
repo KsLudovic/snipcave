6e9895b69a4911a7f7ba577324cf56979f2a5e9f
segolenedemolin <77448148+segolenedemolin@users.noreply.github.com>
2021-06-01 14:25:46 +0200

    Repo & Service for Vote.model

---
+++ src/main/java/fr/aston/snipcave/snipcave/exceptions/VoteNotFoundException.java
@@ -0,0 +1,6 @@
+package fr.aston.snipcave.snipcave.exceptions;
+
+public class VoteNotFoundException extends RuntimeException {
+    public VoteNotFoundException(String no_vote_was_found_) {
+    }
+}
--- src/main/java/fr/aston/snipcave/snipcave/repository/IVoteRepository.java
+++ src/main/java/fr/aston/snipcave/snipcave/repository/IVoteRepository.java
@@ -1,7 +1,14 @@
 package fr.aston.snipcave.snipcave.repository;
 
+import fr.aston.snipcave.snipcave.model.Post;
+import fr.aston.snipcave.snipcave.model.User;
 import fr.aston.snipcave.snipcave.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IVoteRepository extends JpaRepository<Vote,Long> {
+    Optional<Vote> findByUserAndPost(User user, Post post);
+
+    void deleteVoteById(Long id);
 }
