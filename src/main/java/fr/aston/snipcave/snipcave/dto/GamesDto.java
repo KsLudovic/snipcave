eb79762f4bf043f4bd3b8e6911434bf41f7845ef
segolenedemolin <77448148+segolenedemolin@users.noreply.github.com>
2021-06-02 10:13:52 +0200

    Games Dto

---
+++ src/main/java/fr/aston/snipcave/snipcave/dto/GamesDto.java
@@ -0,0 +1,15 @@
+package fr.aston.snipcave.snipcave.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GamesDto {
+    private long id;
+    private String name;
+    private boolean multiplayer;
+    private int numberMaxPlayer;
+}
